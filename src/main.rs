43df8b7ccfa1ca11438b40e4137818bdb5979e16
Adromidous <t24kumar17@gmail.com>
2025-05-28 15:05:48 -0400

    Freestanding binary working - cross-compiled to embedded ARM with no OS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![no_std]
-
-fn main() {}
+#![no_main]
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! { //Implementing our own version of the panic handler.
+	loop {}
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
 	loop {}
 }
