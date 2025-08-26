bb19e8a2ca950c6ba4d0a64b24c503bd36ea3bcb
Adromidous <t24kumar17@gmail.com>
2025-08-25 20:08:03 -0400

    Added vga buffer to print hello world to screen

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!"; //We don't have access to any String::from functions so we
+                                       //must create an array of characters on our own.
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! { //Implementing our own version of the panic handler.
 	loop {}
@@ -11,5 +14,14 @@ fn panic(_info: &PanicInfo) -> ! { //Implementing our own version of the panic h
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-	loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; //EACH INCREMENT, WE MOVE 2 BYTES
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
