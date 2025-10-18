de988ff2bc3ace649b0c8be4a42ee70978e431fb
Adromidous <t24kumar17@gmail.com>
2025-10-17 23:49:58 -0400

    Initial commit towards creating vga interface

--- src/main.rs
+++ src/main.rs
@@ -26,23 +26,5 @@ pub extern "C" fn _start() -> ! {
         }
     }
 
-    // #[cfg(test)]
-    // test_main();
-
     loop {}
 }
-
-// #[cfg(test)]
-// pub fn test_runner(tests: &[&dyn Fn()]) {
-//     println!("Running {} tests", tests.len());
-//     for test in tests {
-//         test();
-//     }
-// }
-
-// #[test_case]
-// fn trivial_assertion() {
-//     println!("trivial assertion");
-//     assert_eq(1, 1);
-//     println!("[ok]");
-// }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
-
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)] //Store as 8 bits, u4 doesn't exist
 pub enum Color {
     Black = 0,
@@ -17,4 +18,35 @@ pub enum Color {
     Pink = 13,
     Yellow = 14,
     White = 15
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)] //Used on structs/enums with one field.
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground_color: Color, background_color: Color) -> ColorCode {
+        ColorCode(((background_color as u8) << 4) | (foreground_color as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
 }
